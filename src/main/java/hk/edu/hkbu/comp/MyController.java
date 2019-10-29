31a0b614a87410e0a8118eb96654e898de9fb843
laisuki1109 <laisuki1109@gmail.com>
2019-10-29 16:16:03 +0800

    suki commit

--- src/main/java/hk/edu/hkbu/comp/MyController.java
+++ src/main/java/hk/edu/hkbu/comp/MyController.java
@@ -29,9 +29,11 @@ public class MyController {
 	@ResponseBody
 	String load(HttpServletRequest request) throws InterruptedException {
 	    String str = ((request.getQueryString()).replaceFirst("^query=*", "")).replace("+"," "); 
-	 		    
-	   // System.out.println(request.getQueryString());
-	     return "Hello <br> <h1>You are searching "+ str +"</h1>" + handleUserRequest(str) ;
+	    
+	    str = str.replaceAll("%28", "(");
+		str = str.replaceAll("%29", ")");	
+	   
+	     return "Hello <br> <h1>You are searching "+ str +"</h1>" + response(handleUserRequest(res(str)) );
 	}
 
 	
@@ -44,9 +46,7 @@ public class MyController {
 	         keywordUrlMap = (HashMap) ois.readObject();
 	         ois.close();
 	         fis.close();
-//	         for (String i : keywordUrlMap.keySet()) {
-//					System.out.println("key: " + i + " value: " + keywordUrlMap.get(i));
-//				}
+
 	      }catch(IOException ioe)
 	      {
 	         ioe.printStackTrace();
@@ -60,7 +60,9 @@ public class MyController {
 	}
 	
 	List<UrlObject> combineANDList(List<UrlObject> temp1, List<UrlObject> temp2 ){
+		
 		for(int j=0; j<temp2.size();j++) {
+			
 			if(temp1.contains(temp2.get(j))) {
 				// duplicate url, combine them by adding the number Appear
 				temp1.get(temp1.indexOf(temp2.get(j))).numberAppear+=temp2.get(j).numberAppear;
@@ -68,8 +70,10 @@ public class MyController {
 		}
 		for (int j=0; j<temp1.size();j++) {
 			if(!temp2.contains(temp1.get(j))) {
+				
 				// duplicate url, combine them by adding the number Appear
 				temp1.remove(temp1.get(j));
+				j--;
 			}
 		}
 		return temp1;
@@ -86,50 +90,90 @@ public class MyController {
 		return temp1;
 	}
 	
-	String handleUserRequest(String str) {
+	List<UrlObject> handleUserRequest(String[] input) {
 		readData();
-		String[] input =str.split(" ");
 		Set<UrlObject> temp = keywordUrlMap.get(input[0]);
-		List<UrlObject> result = new ArrayList<UrlObject>(temp);
-		
+		List<UrlObject> result = new ArrayList<UrlObject>();
+		if(temp!=null) {
+			result = new ArrayList<UrlObject>(temp);
+			
+		}
 		int i =1;
 		while(input.length>i) {
 			if(input[i].equals("AND")|| (!input[i].equals("OR") && !input[i].equals("NOT"))){
 				if(input[i].equals("AND")) {
+					if(input[i+1].contains("(")) {
+						List<UrlObject> result2 = handleUserRequest(release(input[i+1]));
+						result = combineANDList(result,result2);
+						i = i+2;
+					}else {
+						Set<UrlObject> temp2 = keywordUrlMap.get(input[i+1]);
+						List<UrlObject> result2 = new ArrayList<UrlObject>(temp2);
+						//combine the arraylist
+							result = combineANDList(result,result2);
+							i = i+2;
+					}
+					
+					
+				}else { //if other words
+					if(input.length>i) {
+					if(input[i].contains("(")) {
+						List<UrlObject> result2 = handleUserRequest(release(input[i]));
+						result = combineANDList(result,result2);
+						i = i+1;
+					}else {
+						Set<UrlObject> temp2 = keywordUrlMap.get(input[i]);
+						List<UrlObject> result2 = new ArrayList<UrlObject>(temp2);
+						//combine the arraylist
+							result = combineANDList(result,result2);
+							i = i+1;
+						
+					}
+					}
+				}
+			}else if(input[i].equals("OR")){
+				if( input[i+1].contains("(")) {
+					List<UrlObject> result2 = handleUserRequest(release(input[i+1]));
+					result = combineORList(result,result2);
+					i = i+2;
+				}else {
 					Set<UrlObject> temp2 = keywordUrlMap.get(input[i+1]);
 					List<UrlObject> result2 = new ArrayList<UrlObject>(temp2);
 					//combine the arraylist
-						result = combineANDList(result,result2);
+						result = combineORList(result,result2);
 						i = i+2;
-					
+				}
+				
+			}else if (input[i].equals("NOT")) {
+				if(input[i+1].contains("(")) {
+					List<UrlObject> result2 = handleUserRequest(release(input[i+1]));
+					for(int j=0; j<result2.size();j++) {
+						if(result.contains(result2.get(j))) {
+							// remove
+							result.remove(result.indexOf(result2.get(j)));
+						}
+					}
+					i = i+2;
 				}else {
-					//continous inputting keywords
-					Set<UrlObject> temp2 = keywordUrlMap.get(input[i]);
+					Set<UrlObject> temp2 = keywordUrlMap.get(input[i+1]);
 					List<UrlObject> result2 = new ArrayList<UrlObject>(temp2);
 					//combine the arraylist
-					result = combineANDList(result,result2);
-					i = i+1;
-				}
-			}else if(input[i].equals("OR")){
-				Set<UrlObject> temp2 = keywordUrlMap.get(input[i+1]);
-				List<UrlObject> result2 = new ArrayList<UrlObject>(temp2);
-				//combine the arraylist
-				result = combineORList(result,result2);
-				i = i+2;
-			}else if (input[i].equals("NOT")) {
-				Set<UrlObject> temp2 = keywordUrlMap.get(input[i+1]);
-				List<UrlObject> result2 = new ArrayList<UrlObject>(temp2);
-				//delete result2 from result
-				for(int j=0; j<result2.size();j++) {
-					if(result.contains(result2.get(j))) {
-						// duplicate url, combine them by adding the number Appear
-						result.remove(result.indexOf(result2.get(j))).numberAppear+=result2.get(j).numberAppear;
+					for(int j=0; j<result2.size();j++) {
+						if(result.contains(result2.get(j))) {
+							// remove
+							result.remove(result.indexOf(result2.get(j)));
+						}
 					}
+						i = i+2;
 				}
-				i = i+2;
+				
 			}
 		}
-		Collections.sort(result, new Comparator<UrlObject>() {
+
+		return result;
+	}
+	String response(List<UrlObject> input) {
+		Collections.sort(input, new Comparator<UrlObject>() {
 	       @Override
 			public int compare(UrlObject o1, UrlObject o2) {
 				// TODO Auto-generated method stub
@@ -138,8 +182,8 @@ public class MyController {
 	    });
 		
 		String finalResult ="";
-		if(result.size()>1) {
-		for (UrlObject t : result) {
+		if(input.size()>1) {
+		for (UrlObject t : input) {
 			finalResult += t.toString()+"<br>";
 		}
 		}else {
@@ -147,30 +191,39 @@ public class MyController {
 		}
 		return finalResult;
 	}
-	String res(String str) {
+	String[] res(String str) {
 		String fomatStr = str;
 		
 		//print out what we have input;
-		fomatStr = fomatStr.replace("%28", "(");
-		fomatStr = fomatStr.replace("%29", ")");		
+		fomatStr = fomatStr.replaceAll("%28", "(");
+		fomatStr = fomatStr.replaceAll("%29", ")");		
 		System.out.println(fomatStr);	
 		
 		ArrayList<String> groupitem = new ArrayList<String>();
 		
 		Matcher m = Pattern.compile("(\\([^)]+\\)|\\w+)").matcher(fomatStr);		   		 	
         while(m.find()) {	    	 	    		    	   	 
-	    	 System.out.println("Found bracket from index "+ m.start() +" to "+ (m.end()-1));
+	    	 // System.out.println("Found bracket from index "+ m.start() +" to "+ (m.end()-1));
 	    	 String temp= String.format(m.group(0));	    	 
 	    	 groupitem.add(temp);
-	    	 
-	    	 //groupitem.add(m.start());
-	    	//groupitem.add(m.end()-1);	    	 	    	
+	    	    	 	    	
 	}       	
         String.format(fomatStr);   
         System.out.println("groupitem: ");
         System.out.println(groupitem);
-		return fomatStr; 
-		
+        //change it to array
+        String[] input = new String[groupitem.size()];
+        for (int i = 0; i<groupitem.size();i++) {
+        	input[i]=groupitem.get(i);
+        }
+		return input; 
+	}
+	
+	String[] release(String str) {
+		//release the backets
+		str= str.substring(1,str.length()-1);
+		String[] input = str.split(" ");
+		return input;
 	}
 	
     @GetMapping("greeting")
